dcd55521996927066c5352c22ee0c30365235255
GustaKowai <gusta.kowai@gmail.com>
2022-08-07 01:36:10 -0300

    Adiciona o cliente na conta.

--- src/Conta.java
+++ src/Conta.java
@@ -6,10 +6,13 @@ public abstract class Conta implements IConta{
     protected int agencia;
     protected int numeroConta;
     protected double saldo;
+    protected Cliente cliente;
 
-    public Conta() {
+    public Conta(Cliente cliente) {
         this.agencia = Conta.AGENCIA_PADRAO;
         this.numeroConta = SEQUENCIAL++;
+        this.cliente = cliente;
+
     }
 
     public int getAgencia() {
@@ -42,6 +45,7 @@ public abstract class Conta implements IConta{
     }
 
     protected void imprimirAtributos() {
+        System.out.println(String.format("Titular: %s", this.cliente.getNome()));
         System.out.println(String.format("Agencia: %d", this.agencia));
         System.out.println(String.format("Numero da conta: %d", this.numeroConta));
         System.out.println(String.format("Saldo: %.2f", this.saldo));
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,17 @@ import org.w3c.dom.ls.LSOutput;
 public class Main {
 
     public static void main(String[] args) {
-        Conta cc = new ContaCorrente();
-        Conta cp = new ContaPoupanca();
+
+        Cliente gustavo = new Cliente();
+        gustavo.setNome("Gustavo");
+        Conta cc = new ContaCorrente(gustavo);
+        Conta cp = new ContaPoupanca(gustavo);
 
         cc.depositar(100.0);
 
         cc.transferir(cp,100.0);
         cp.transferir(cc,50.0);
+        cc.sacar(30.0);
 
         cc.extrato();
         cp.extrato();
